62f65911127b1f46492e9b72cb4ea619214dfa3e
Andrew Stoeckley <andrewjstoeckley@gmail.com>
2018-03-20 14:20:24 +0100

    0.1.62 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nl.balconystudio/clojure-assistant "0.1.61"
+(defproject nl.balconystudio/clojure-assistant "0.1.62"
   :description "Simple tools for structural validation of data shapes and debug mode assertions."
   :url "http://github.com/astoeckley/clojure-assistant"
   :license {:name "Eclipse Public License"
